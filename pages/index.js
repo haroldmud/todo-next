1ad5fd0d4cd4065b5deff92111d1a78124ecd884
haroldmud <haroldmudosa@gmail.com>
2023-03-03 18:17:00 +0200

    Feat: multiple selection

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,6 @@ export default function Home() {
     setCheck(-1)
   }
 
-  // function HandleEdit(i)
-
   return (
    <section className="flex justify-center">
       <div className="flex flex-col w-5/12 h-[100vh] justify-center">
@@ -77,19 +75,20 @@ export default function Home() {
                       <div className="flex flex-col justify-center" onClick={()=>{
                         if(check !== idx) {
                           setCheck(idx)
+                          collection.checked = true
                         }else{
                           setCheck(null)
+                          collection.checked = false
                         }
                       }}>
-                        <button className={`${idx ===check ? "hidden" : ""}`}><RiCheckboxBlankCircleLine/></button>
-                        <button className={`${idx === check ? "" : "hidden"}`}><RiCheckboxCircleLine/></button>
+                        {
+                          collection.checked== !collection.checke ? <RiCheckboxCircleLine/> : <RiCheckboxBlankCircleLine/>
+                        }
+                        {/* <button className={`${ collection.checked== !collection.checked ? "hidden" : ""}`}><RiCheckboxBlankCircleLine/></button>
+                        <button className={`${ collection.checked== !collection.checked ? "" : "hidden"}`}><RiCheckboxCircleLine/></button> */}
                       </div>
                       {/* check ? "line-through" : "" */}
-                      <p className={`${idx === check ? "line-through":"underline" }`}>{
-                      
-                      idx === edit && newItem.split("").length > 0 ? collection.text = newItem  : collection.text
-                      
-                      }</p>  
+                      <p className={`${idx === check || collection.checked==true ? "line-through":"underline" }`}>{ idx === edit && newItem.split("").length > 0 ? collection.text = newItem  : collection.text}</p>  
                     </label>
                     <div className={`${idx === edit ? "flex" :"hidden"} absolute w-full justify-center bg-slate-400 border -ml-1 gap-8`}>
                       <input
@@ -97,12 +96,11 @@ export default function Home() {
                         onChange={(e)=>setNewItem(e.target.value)}
                         className="w-9/12 placeholder:text-gray-500  placeholder:italic placeholder:text-[0.7rem] bg-slate-400 outline-none pl-8 border-blue-500 z-10 "  type="text" />
                       <div>
-                      {/* <button onClick={()=>{setNewEdit(false)}} className="my-auto text-[0.9rem]"><RxCross2/></button> */}
-                      <button onClick={()=>{setEdit(null); setCheck(null); setNewItem("")}} className="my-auto"><BiCheck/></button>
+                      <button onClick={()=>{setEdit(null); setCheck(null); setNewItem(""); collection.checked = false}} className="my-auto"><BiCheck/></button>
                       </div>
                     </div>
                   <div className="flex  justify-center gap-1">
-                    <button className={check === idx ? "block text-[0.75rem] my-auto" : "hidden"} onClick={()=>{
+                    <button className={check === idx || collection.checked == true ? "block text-[0.75rem] my-auto" : "hidden"} onClick={()=>{
                       if(edit !== idx){
                         setEdit(idx);
                         setNewItem(collection.text)
@@ -110,7 +108,7 @@ export default function Home() {
                         setEdit(null)
                       }
                     }}><AiOutlineEdit/></button>
-                    <button className={check === idx ? "block" : "hidden"} onClick={()=>handleDeletion(idx)}><GrFormTrash/></button>
+                    <button className={check === idx || collection.checked == true ? "block" : "hidden"} onClick={()=>handleDeletion(idx)}><GrFormTrash/></button>
                   </div></li>
                   )
                 }
